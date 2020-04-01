8ffda1c101358f19f08cc9527785b3c01f484a5e
MrTroble <nifroh56@gmail.com>
2020-04-01 15:26:34 +0200

    Fixed ambiguety with dependencies

--- TGEditor/ShaderTool.cpp
+++ TGEditor/ShaderTool.cpp
@@ -1,17 +1,15 @@
 #include "ShaderTool.hpp"
 #include <iostream>
-#include <string>
-
-using namespace std;
+#include <string.h>
 
 namespace shadertool {
 
 	int exec(const char* command) {
 		char commandLineToRun[256];
 		commandLineToRun[0] = '\0';
-		strcat(commandLineToRun, SHADERTOOL_PATH);
-		strcat(commandLineToRun, " ");
-		strcat(commandLineToRun, command);
-		return system(commandLineToRun);
+		std::strcat(commandLineToRun, SHADERTOOL_PATH);
+		std::strcat(commandLineToRun, " ");
+		std::strcat(commandLineToRun, command);
+		return std::system(commandLineToRun);
 	}
 }
