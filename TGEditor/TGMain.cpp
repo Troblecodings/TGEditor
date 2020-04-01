90a127799b7fb5703c866349348c69e31e634836
Clazilla <47395364+Clazilla@users.noreply.github.com>
2020-04-01 17:26:50 +0200

    added titlebar

--- TGEditor/TGMain.cpp
+++ TGEditor/TGMain.cpp
@@ -27,13 +27,18 @@ int main() {
 	shadertool::exec("map make TGEditor"); // Recompile before loading
 	loadResourceFile("Resources/TGEditor.tgr");
 
-	const char* chars[] = { "TEXTURES", "MATERIALS", "ACTORS" };
+	const char* chars[] = { "TEXTURES", "MATERIALS", "ACTORS", "File", "Edit", "Project", "Settings"};
 	glm::mat4 materials[] = {
 		tge::drw::genMatrix(-0.97f, -0.85f, -0.5f, 0.08f, 0.08f),
 		tge::drw::genMatrix(-0.97f, -0.2f, -0.5f, 0.08f, 0.08f),
-		tge::drw::genMatrix(-0.97f, 0.5f, -0.5f, 0.08f, 0.08f)
+		tge::drw::genMatrix(-0.97f, 0.5f, -0.5f, 0.08f, 0.08f),
+
+		tge::drw::genMatrix(-0.95f, -0.97f, -0.5f, 0.03f, 0.04f),
+		tge::drw::genMatrix(-0.88f, -0.97f, -0.5f, 0.03f, 0.04f),
+		tge::drw::genMatrix(-0.81f, -0.97f, -0.5f, 0.03f, 0.04f),
+		tge::drw::genMatrix(-0.70f, -0.97f, -0.5f, 0.03f, 0.04f)
 	};
-	tge::fnt::createStringActor(tge::fnt::fonts.data(), chars, 3, materials);
+	tge::fnt::createStringActor(tge::fnt::fonts.data(), chars, 7, materials);
 
 	startTGEngine();
 
