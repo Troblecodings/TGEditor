224d0ddc38d17e59fe7f251d4419068a75a82b0c
MrTroble <nifroh56@gmail.com>
2020-05-05 19:24:15 +0200

    Added administration lists

--- TGEditor/TGMain.cpp
+++ TGEditor/TGMain.cpp
@@ -1,4 +1,3 @@
-#include "TGMain.h"
 #include "ShaderTool.hpp"
 #include <TGEngine.hpp>
 #include <gamecontent/PlayerController.hpp>
@@ -6,10 +5,8 @@
 #include <pipeline/buffer/Texturebuffer.hpp>
 #include <gamecontent/camera/2DCamera.hpp>
 #include <io/Resource.hpp>
-#include <io/Font.hpp>
 #include <drawlib/Quad.hpp>
-#include <array>
-#include <pipeline/buffer/UniformBuffer.hpp>
+#include "Administration.hpp"
 
 using namespace tge::io;
 using namespace tge::gmc;
@@ -17,16 +14,6 @@ using namespace tge::tex;
 using namespace tge::buf;
 
 static TopDownCamera camera;
-
-bool flag = false;
-std::array<float, 784> uniformData {
-	1, 0, 0, 0,
-	0, 1, 0, 0,
-	0, 0, 0.1, 0,
-	0, 0, 0, 1
-};
-uint32_t currentSelectedActor = 0;
-std::vector<char*> actorNames;
 
 static void contoller(Input in) {}
 
@@ -45,34 +32,31 @@ int main() {
 
 	loadResourceFile("Resources/TGEditor.tgr");
 
-	for (size_t i = 16; i < 16 + 128 * 4; i += 4) {
-		uniformData[i + 2] = 1;
-		uniformData[i + 3] = 1;
-	}
-
-	fillUniformBuffer(TRANSFORM_BUFFER, uniformData.data(), uniformData.size() * sizeof(float));
-	fillUniformBuffer(TRANSFORM_BUFFER_2, uniformData.data(), uniformData.size() * sizeof(float));
-
 	const char* chars[] = { "TEXTURES", "MATERIALS", "ACTORS", 
 	"Name", "Name", "Name",
 	"File", "Edit", "Project", "Settings"
 };
 	glm::mat4 materials[] = {
-		tge::drw::genMatrix(-0.97f, -0.88f, -0.5f, 0.05f, 0.07f),
-		tge::drw::genMatrix(-0.97f, -0.23f, -0.5f, 0.05f, 0.07f),
-		tge::drw::genMatrix(-0.97f, 0.47f, -0.5f, 0.05f, 0.07f),
+		tge::drw::genMatrix(-0.97f, -0.88f, -0.5f, 0.07f, 0.07f),
+		tge::drw::genMatrix(-0.97f, -0.23f, -0.5f, 0.07f, 0.07f),
+		tge::drw::genMatrix(-0.97f, 0.47f, -0.5f, 0.07f, 0.07f),
 
-		tge::drw::genMatrix(-0.97f, -0.795f, -0.5f, 0.025f, 0.04f),
-	    tge::drw::genMatrix(-0.97f, -0.145f, -0.5f, 0.025f, 0.04f),
-		tge::drw::genMatrix(-0.97f, 0.555f, -0.5f, 0.025f, 0.04f),
+		tge::drw::genMatrix(-0.97f, -0.795f, -0.5f, 0.04f, 0.04f),
+	    tge::drw::genMatrix(-0.97f, -0.145f, -0.5f, 0.04f, 0.04f),
+		tge::drw::genMatrix(-0.97f, 0.555f, -0.5f, 0.04f, 0.04f),
 
-		tge::drw::genMatrix(-0.95f, -0.97f, -0.5f, 0.03f, 0.04f),
-		tge::drw::genMatrix(-0.88f, -0.97f, -0.5f, 0.03f, 0.04f),
-		tge::drw::genMatrix(-0.81f, -0.97f, -0.5f, 0.03f, 0.04f),
-		tge::drw::genMatrix(-0.70f, -0.97f, -0.5f, 0.03f, 0.04f)
+		tge::drw::genMatrix(-0.95f, -0.97f, -0.5f, 0.04f, 0.04f),
+		tge::drw::genMatrix(-0.88f, -0.97f, -0.5f, 0.04f, 0.04f),
+		tge::drw::genMatrix(-0.81f, -0.97f, -0.5f, 0.04f, 0.04f),
+		tge::drw::genMatrix(-0.70f, -0.97f, -0.5f, 0.04f, 0.04f)
 	};
 	constexpr uint32_t size = sizeof(chars) / sizeof(*chars);
 	tge::fnt::createStringActor(tge::fnt::fonts.data(), chars, size, materials);
+
+
+	// Must be last statement
+	loadAdministration();
+	administration();
 
 	startTGEngine();
 
